2ed9eea705c5b80075c798df071dfa322511dd26
Alena Khineika <alena.khineika@gmail.com>
2019-02-06 13:12:27 +0100

    Compass no longer supports the visual rule builder for server versions below 3.2

--- src/components/compass-schema-validation/compass-schema-validation.jsx
+++ src/components/compass-schema-validation/compass-schema-validation.jsx
@@ -19,13 +19,24 @@ import { namespaceChanged } from 'modules/namespace';
 import { openLink } from 'modules/link';
 import { fetchSampleDocuments } from 'modules/sample-documents';
 import { changeZeroState, zeroStateChanged } from 'modules/zero-state';
+import semver from 'semver';
 
 import styles from './compass-schema-validation.less';
 
 /**
- * Warning for the status row.
+ * The lowest supported version.
+ */
+const MIN_VERSION = '3.2.0';
+
+/**
+ * Read only warning for the banner.
  */
 const READ_ONLY_WARNING = 'Schema validation on readonly views are not supported.';
+
+/**
+ * Version warning for the banner.
+ */
+const VERSION_WARNING = 'Compass no longer supports the visual rule builder for server versions below 3.2. To use the visual rule builder, please';
 
 /**
  * Header for zero state.
@@ -40,7 +51,12 @@ const SUBTEXT = 'Create rules to enforce data structure of documents on updates 
 /**
  * Link to the schema validation documentation.
  */
-const DOCUMENTATION_LINK = 'https://docs.mongodb.com/manual/core/schema-validation/';
+const DOC_SCHEMA_VALIDATION = 'https://docs.mongodb.com/manual/core/schema-validation/';
+
+/**
+ * Link to the upgrading to the latest revision documentation.
+ */
+const DOC_UPGRADE_REVISION = 'https://docs.mongodb.com/manual/tutorial/upgrade-revision/';
 
 /**
  * The core schema validation component.
@@ -52,17 +68,52 @@ class CompassSchemaValidation extends Component {
     isZeroState: PropTypes.bool.isRequired,
     changeZeroState: PropTypes.func.isRequired,
     zeroStateChanged: PropTypes.func.isRequired,
-    isEditable: PropTypes.bool.isRequired
+    isEditable: PropTypes.bool.isRequired,
+    openLink: PropTypes.func.isRequired,
+    serverVersion: PropTypes.string.isRequired
   }
 
   /**
-   * Render banner with information.
+   * Checks if it is a proper server version.
+   *
+   * @returns {Boolean}
+   */
+  isProperServerVersion() {
+    return semver.gte(this.props.serverVersion, MIN_VERSION);
+  }
+
+  /**
+   * Checks if the zero state window should be displayed.
+   *
+   * @returns {Boolean}
+   */
+  checkIfZeroState() {
+    return (this.props.isZeroState || !this.props.isEditable);
+  }
+
+  /**
+   * Renders the banner if the validatiion is not editable.
    *
    * @returns {React.Component} The component.
    */
   renderBanner() {
     if (!this.props.isEditable) {
-      return (<StatusRow style="warning">{READ_ONLY_WARNING}</StatusRow>);
+      if (this.isProperServerVersion()) {
+        return (<StatusRow style="warning">{READ_ONLY_WARNING}</StatusRow>);
+      }
+
+      return (
+        <StatusRow style="warning">
+          {VERSION_WARNING}
+          <div>&nbsp;</div>
+          <a
+            className={classnames(styles['upgrade-link'])}
+            onClick={this.props.openLink.bind(this, DOC_UPGRADE_REVISION)}
+          >
+            upgrade to MongoDB 3.2.
+          </a>
+        </StatusRow>
+      );
     }
   }
 
@@ -72,7 +123,7 @@ class CompassSchemaValidation extends Component {
    * @returns {React.Component} The component.
    */
   renderZeroState() {
-    if (this.props.isZeroState || !this.props.isEditable) {
+    if (this.checkIfZeroState()) {
       return (
           <div className={classnames(styles['zero-state-container'])}>
             <ZeroGraphic />
@@ -88,7 +139,7 @@ class CompassSchemaValidation extends Component {
                 </div>
                 <a
                   className={classnames(styles['zero-state-link'])}
-                  href={DOCUMENTATION_LINK}
+                  onClick={this.props.openLink.bind(this, DOC_SCHEMA_VALIDATION)}
                 >
                   Learn more about validations
                 </a>
@@ -105,7 +156,7 @@ class CompassSchemaValidation extends Component {
    * @returns {React.Component} The component.
    */
   renderContent() {
-    if (!this.props.isZeroState && this.props.isEditable) {
+    if (!this.checkIfZeroState()) {
       return (
         <div className={classnames(styles['content-container'])}>
           <ValidationEditor {...this.props} />
